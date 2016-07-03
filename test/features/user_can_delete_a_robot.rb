0a3b79610b9caae7035268b8a6e7d52994d17807
David Tinianow <david.tinianow@gmail.com>
2016-07-03 17:14:20 -0600

    made it look nice

--- test/features/user_can_delete_a_robot.rb
+++ test/features/user_can_delete_a_robot.rb
@@ -5,9 +5,7 @@ class UserCanDeleteARobotTest < FeatureTest
   def test_user_can_delete_a_robot
     robot_world.create(name: "Bob")
     visit '/robots'
-    within('#robot-name') do
-      assert page.has_content?("Bob")
-    end
+    assert page.has_content?("Bob")
     click_on("Delete")
     assert_equal '/robots', current_path
     refute page.has_css?('#robot-name')
